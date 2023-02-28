c7b03038e61120a1424ee198ec685c1c9a355c10
therealeddy <eduardo.mfonseca@hotmail.com>
2023-02-28 18:00:54 -0300

    feat: add LoginValidationFactory

---
+++ src/main/factories/login/login-validation.ts
@@ -0,0 +1,16 @@
+import { RequiredFieldValidation } from '../../../presentation/helpers/validatiors/required-field-validation'
+import { type Validation } from '../../../presentation/helpers/validatiors/validation'
+import { ValidationComposite } from '../../../presentation/helpers/validatiors/validation-composite'
+import { EmailValidation } from '../../../presentation/helpers/validatiors/email-validation'
+import { EmailValidatorAdapter } from '../../../utils/email-validator-adapter'
+
+export const makeLoginValidation = (): ValidationComposite => {
+  const validations: Validation[] = []
+
+  for (const field of ['email', 'password']) {
+    validations.push(new RequiredFieldValidation(field))
+  }
+
+  validations.push(new EmailValidation('email', new EmailValidatorAdapter()))
+  return new ValidationComposite(validations)
+}
--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,34 +1,26 @@
-import { type HttpRequest, type HttpResponse, type Controller, type EmailValidator, type Authentication } from './login-protocols'
-import { InvalidParamError, MissingParamError } from '../../errors'
+import { type HttpRequest, type HttpResponse, type Controller, type Validation, type Authentication } from './login-protocols'
 import { anauthorized, badRequest, ok, serverError } from '../../helpers/http-helper'
 
 export class LoginController implements Controller {
-  private readonly emailValidator: EmailValidator
+  private readonly validation: Validation
   private readonly authentication: Authentication
 
-  constructor (emailValidator: EmailValidator, authentication: Authentication) {
-    this.emailValidator = emailValidator
+  constructor (authentication: Authentication, validation: Validation) {
+    this.validation = validation
     this.authentication = authentication
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const requiredFields = ['email', 'password']
+      const error = this.validation.validate(httpRequest.body)
 
-      for (const filed of requiredFields) {
-        if (!httpRequest.body[filed]) {
-          return badRequest(new MissingParamError(filed))
-        }
+      if (error) {
+        return badRequest(error)
       }
 
       const { email, password } = httpRequest.body
-
-      const isValid = this.emailValidator.isValid(email)
-
-      if (!isValid) {
-        return await new Promise(resolve => { resolve(badRequest(new InvalidParamError('email'))) })
-      }
       const accessToken = await this.authentication.auth(email, password)
+
       if (!accessToken) {
         return anauthorized()
       }
