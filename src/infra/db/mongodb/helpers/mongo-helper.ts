f4ee0de9212d01eef4761edb8e6f8eae9c7eb058
therealeddy <eduardo.mfonseca@hotmail.com>
2023-03-02 17:36:27 -0300

    feat: ensure AccountMongoRepository updates the account accessToken on updateAccessToken success

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,4 +1,4 @@
-import { type Collection, MongoClient } from 'mongodb'
+import { type Collection, MongoClient, ObjectId } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient,
@@ -24,5 +24,9 @@ export const MongoHelper = {
   map (collection: any): any {
     const { _id, ...collectionWithoutId } = collection
     return Object.assign({}, collectionWithoutId, { id: _id.toHexString() })
+  },
+
+  parseToObjectId (value: string): ObjectId {
+    return new ObjectId(value)
   }
 }
