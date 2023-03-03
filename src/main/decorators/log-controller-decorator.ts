7fe3111a1a385143c82500c6e6c81ef1d70e0674
therealeddy <eduardo.mfonseca@hotmail.com>
2023-03-03 15:08:49 -0300

    refactor: remove duplicated code

--- src/main/decorators/log-controller-decorator.ts
+++ src/main/decorators/log-controller-decorator.ts
@@ -2,13 +2,10 @@ import { type Controller, type HttpResponse, type HttpRequest } from '../../pres
 import { type LogErrorRepository } from '../../data/protocols/db/log/log-error-repository'
 
 export class LogControllerDecorator implements Controller {
-  private readonly controller: Controller
-  private readonly logErrorRepository: LogErrorRepository
-
-  constructor (controller: Controller, logErrorRepository: LogErrorRepository) {
-    this.controller = controller
-    this.logErrorRepository = logErrorRepository
-  }
+  constructor (
+    private readonly controller: Controller,
+    private readonly logErrorRepository: LogErrorRepository
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const httpResponse = await this.controller.handle(httpRequest)
