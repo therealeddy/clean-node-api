bc7213bd818067a31e830e8a2da4eda1cda21e44
therealeddy <eduardo.mfonseca@hotmail.com>
2023-03-16 19:40:08 -0300

    feat: ensure AuthMiddleware returns 403 if no headers are found

--- src/presentation/protocols/middleware.ts
+++ src/presentation/protocols/middleware.ts
@@ -1,5 +1,5 @@
 import { type HttpRequest, type HttpResponse } from './http'
 
-export interface Controller {
+export interface Middleware {
   handle: (httpRequest: HttpRequest) => Promise<HttpResponse>
 }
