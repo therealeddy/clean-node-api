d5b2726594e592b4fe4e6ad17f475f5513ae414d
therealeddy <eduardo.mfonseca@hotmail.com>
2023-03-29 21:39:21 -0300

    feat: ensure SurveyResultMongoRepository insert new surveyResult if its new

---
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
@@ -0,0 +1,26 @@
+import { type SaveSurveyResultModel, type SurveyResultModel, type SaveSurveyResultRepository } from '~/data/usecases/save-survey-result/db-save-survey-result-protocols'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
+  async save (data: SaveSurveyResultModel): Promise<SurveyResultModel> {
+    const surveyResultCollection = await MongoHelper.getCollection('surveyResults')
+    await surveyResultCollection.findOneAndUpdate({
+      surveyId: data.surveyId,
+      accountId: data.accountId
+    }, {
+      $set: {
+        answer: data.answer,
+        date: data.date
+      }
+    }, {
+      upsert: true
+    })
+
+    const surveyResult = await surveyResultCollection.findOne({
+      surveyId: data.surveyId,
+      accountId: data.accountId
+    })
+
+    return MongoHelper.map(surveyResult)
+  }
+}
