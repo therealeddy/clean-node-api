03b1ba5137bc21f3c19783efda28a285ffe3d724
therealeddy <eduardo.mfonseca@hotmail.com>
2023-04-06 01:16:13 -0300

    docs: update login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,37 @@
+import { loginPath } from './paths'
+import { badRequest, serverError, unauthorized, notFound } from './components'
+
+import { accountSchema, loginParamsSchema, errorSchema } from './schemas'
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Clean Node API',
     description: 'API do curso do Mango para realizar enquetes entre programadores',
     version: '1.0.0'
+  },
+  license: {
+    name: 'GPL-3.0-or-later',
+    url: 'https://spdx.org/licenses/GPL-3.0-or-later.html'
+  },
+  servers: [{
+    url: '/api'
+  }],
+  tags: [{
+    name: 'Login'
+  }],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    'login-params': loginParamsSchema,
+    error: errorSchema
+  },
+  components: {
+    badRequest,
+    serverError,
+    unauthorized,
+    notFound
   }
 }
