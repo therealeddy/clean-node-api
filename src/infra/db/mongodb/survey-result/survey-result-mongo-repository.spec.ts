885ed2097d9c0c9d07c93a04c5639ed7392f0518
therealeddy <eduardo.mfonseca@hotmail.com>
2023-04-07 18:06:13 -0300

    fix: change save survey result to return all answers

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
@@ -75,8 +75,11 @@ describe('Survey Result Mongo Repository', () => {
 
       expect(surveyResult).toBeTruthy()
       expect(surveyResult.surveyId).toEqual(MongoHelper.parseToObjectId(survey.id))
+      expect(surveyResult.answers[0].answer).toBe(survey.answers[0].answer)
       expect(surveyResult.answers[0].count).toBe(1)
       expect(surveyResult.answers[0].percent).toBe(100)
+      expect(surveyResult.answers[1].count).toBe(0)
+      expect(surveyResult.answers[1].percent).toBe(0)
     })
 
     test('Should update survey result if its new', async () => {
@@ -103,6 +106,8 @@ describe('Survey Result Mongo Repository', () => {
       expect(surveyResult.answers[0].answer).toBe(survey.answers[1].answer)
       expect(surveyResult.answers[0].count).toBe(1)
       expect(surveyResult.answers[0].percent).toBe(100)
+      expect(surveyResult.answers[1].count).toBe(0)
+      expect(surveyResult.answers[1].percent).toBe(0)
     })
   })
 })
