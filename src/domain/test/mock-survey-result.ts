4d5cb5390efe6ec2244fe91f5369048184a00ba4
therealeddy <eduardo.mfonseca@hotmail.com>
2023-04-09 23:33:58 -0300

    feat: ensure DbLoadSurveyResult returns empty survey result if there is no result

--- src/domain/test/mock-survey-result.ts
+++ src/domain/test/mock-survey-result.ts
@@ -9,17 +9,13 @@ export const mockSaveSurveyResultParams = (): SaveSurveyResultParams => ({
 })
 
 export const mockSurveyResultModel = (): SurveyResultModel => ({
-  surveyId: 'any_survey_id',
+  surveyId: 'any_id',
   question: 'any_question',
   answers: [{
-    answer: 'other_answer',
-    count: 1,
-    percent: 50
-  }, {
-    answer: 'other_answer',
     image: 'any_image',
-    count: 10,
-    percent: 80
+    answer: 'any_answer',
+    count: 0,
+    percent: 0
   }],
   date: new Date()
 })
