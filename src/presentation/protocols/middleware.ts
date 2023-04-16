23d5d210e2a3330c9215899ac8bd4d2ff8b1f259
therealeddy <eddy.developerr@gmail.com>
2023-04-16 01:28:34 -0300

    fix: adjusts script build and remove HttpRequest of Middlewares

--- src/presentation/protocols/middleware.ts
+++ src/presentation/protocols/middleware.ts
@@ -1,5 +1,5 @@
-import { type HttpRequest, type HttpResponse } from './http'
+import { type HttpResponse } from './http'
 
-export interface Middleware {
-  handle: (httpRequest: HttpRequest) => Promise<HttpResponse>
+export interface Middleware<T = any> {
+  handle: (httpRequest: T) => Promise<HttpResponse>
 }
